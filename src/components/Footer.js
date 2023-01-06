dfa6662d60a6dc0590ef7872387941d5a61bc927
Shaho Farivar <56560445+ShahoFarivar@users.noreply.github.com>
2023-01-06 17:34:02 +0330

    add fontawesome icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,28 @@ import './Footer.css'
 
 export default function Footer(){
     return(
-            <footer className="border-top .footer">
-                <div className="container px-4 px-lg-5">
+            <footer className="border-top">
+                <div className="container px-4 px-lg-5 footer-icons">
                     <div className="row gx-4 gx-lg-5 justify-content-center">
                         <div className="col-md-10 col-lg-8 col-xl-7">
-                            <ul className="list-inline text-center footer-icons">
+                            <ul className="list-inline text-center">
                                 <li className="list-inline-item">               
-                                    <Link to={{ pathname: "https://herewecode.io/" }} target="_blank">
-                                        Twitter Icon
+                                    <Link to={{ pathname: "https://minekraft.ir/" }} target="_blank">
+                                    <i className="fa-brands fa-twitter icons"></i>
                                     </Link>    
                                 </li>
                                 <li className="list-inline-item">
-                                    <Link to={{ pathname: "https://herewecode.io/" }} target="_blank">
-                                        Facebook Icon
+                                    <Link to={{ pathname: "https://minekraft.ir/" }} target="_blank">
+                                    <i className="fa-brands fa-facebook icons"></i>
                                     </Link>
                                 </li>
                                 <li className="list-inline-item">
-                                    <Link to={{ pathname: "https://herewecode.io/" }} target="_blank">
-                                        Github Icon 
+                                    <Link to={{ pathname: "https://minekraft.ir/" }} target="_blank">
+                                    <i className="fa-brands fa-github icons"></i>
                                     </Link>
                                 </li>
                             </ul>
-                            <div className="copyright">Copyright © Your Website 2022</div>
+                            <div className="copyright">Copyright © Clean Blog 2023</div>
                         </div>
                     </div>
                 </div>
